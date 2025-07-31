ee53102046525617668008218097c67ce5b78f76
Rijan77 <rijanacharya73@gmail.com>
2025-07-31 17:24:14 +0545

    Add Bootom Form to show the district name

---
+++ lib/weather_app/app/presentation/widgets/distric_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DistricForm extends StatefulWidget {
+  const DistricForm({super.key});
+
+  @override
+  State<DistricForm> createState() => _DistricFormState();
+}
+
+class _DistricFormState extends State<DistricForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
