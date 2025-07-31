b4ce2e8273f08c8b305ca3b1ad9f067734508323
Rijan77 <rijanacharya73@gmail.com>
2025-07-31 17:25:36 +0545

    Modifeid: District_Form

--- lib/weather_app/app/presentation/widgets/distric_form.dart
+++ lib/weather_app/app/presentation/widgets/distric_form.dart
@@ -10,6 +10,6 @@ class DistricForm extends StatefulWidget {
 class _DistricFormState extends State<DistricForm> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Column(children: [Text("rijan")]));
   }
 }
